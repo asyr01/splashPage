9fdba263f58d8e6d1ec78555c91566e25332bc40
example <alisayar0107@gmail.com>
2021-04-12 15:19:04 +0300

    we can toggle between styled body and default

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const { body } = document;
 
 function changeBackground(number) {
+  // Check if background showing
+  let prevBackground;
+  if (body.className) {
+    prevBackground = body.className;
+  }
+  // Reset CSS class for body.
+  body.className = '';
   switch (number) {
     case '1':
       // code block
-      body.classList.add('background-1');
-      break;
+      return prevBackground === 'background-1'
+        ? false
+        : body.classList.add('background-1');
+
     case '2':
-      body.classList.add('background-2');
-      break;
+      return prevBackground === 'background-2'
+        ? false
+        : body.classList.add('background-2');
     case '3':
-      body.classList.add('background-3');
-      break;
+      return prevBackground === 'background-3'
+        ? false
+        : body.classList.add('background-3');
     case '4':
-      body.classList.add('background-4');
-      break;
+      return prevBackground === 'background-4'
+        ? false
+        : body.classList.add('background-4');
     default:
       break;
   }
