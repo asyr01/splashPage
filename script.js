59f5b02df725ff0667530c5761f18d21f102610c
example <alisayar0107@gmail.com>
2021-04-12 21:00:49 +0300

    localStorage functionality added for saving theme.

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const { body } = document;
 
+// Check localstorage if there is bgTheme
+if (localStorage.getItem('bgTheme')) {
+  body.classList.add(localStorage.getItem('bgTheme'));
+}
+
 function changeBackground(number) {
   // Check if background showing
   let prevBackground;
   if (body.className) {
     prevBackground = body.className;
   }
+
   // Reset CSS class for body.
   body.className = '';
+  localStorage.removeItem('bgTheme');
+
   if (!(prevBackground === `background-${number}`)) {
     body.classList.add(`background-${number}`);
+    localStorage.setItem('bgTheme', `background-${number}`);
   }
 }
